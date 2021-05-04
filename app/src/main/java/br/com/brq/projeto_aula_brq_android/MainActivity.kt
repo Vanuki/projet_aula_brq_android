06369611ec84d758e1e04f77d75921c93d315a4f
Rodolfo_Araujo_Vanucchi <rodolfo_vanucchi@hotmail.com>
2021-05-04 16:39:43 -0300

    Fourth Test Alteration

--- app/src/main/java/br/com/brq/projeto_aula_brq_android/MainActivity.kt
+++ app/src/main/java/br/com/brq/projeto_aula_brq_android/MainActivity.kt
@@ -1,11 +1,21 @@
 package br.com.brq.projeto_aula_brq_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val botao = findViewById<Button>(R.id.botao)
+
+        botao.setOnClickListener(){
+            startActivity(Intent(this, SegundaTela::class.java))
+
+        }
     }
 }
