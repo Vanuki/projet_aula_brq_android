f6b0164c6f3f1da82328a8eb0b79e03924876fcd
Rodolfo_Araujo_Vanucchi <rodolfo_vanucchi@hotmail.com>
2021-05-04 16:49:03 -0300

    Fifth Test Alteration

--- app/src/main/java/br/com/brq/projeto_aula_brq_android/SegundaTela.kt
+++ app/src/main/java/br/com/brq/projeto_aula_brq_android/SegundaTela.kt
@@ -1,11 +1,21 @@
 package br.com.brq.projeto_aula_brq_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SegundaTela : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda_tela)
+
+        val botao = findViewById<Button>(R.id.botao)
+
+        botao.setOnClickListener(){
+            startActivity(Intent(this, TerceiraTela::class.java))
+        }
+
+
     }
 }
---
+++ app/src/main/java/br/com/brq/projeto_aula_brq_android/TerceiraTela.kt
@@ -0,0 +1,11 @@
+package br.com.brq.projeto_aula_brq_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TerceiraTela : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terceira_tela)
+    }
+}
