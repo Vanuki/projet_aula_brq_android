60b7d8ba2830373789f97788e2c5ad07349c99aa
Rodolfo_Araujo_Vanucchi <rodolfo_vanucchi@hotmail.com>
2021-05-05 09:32:27 -0300

    Sixth Test Alteration

--- app/src/main/java/br/com/brq/projeto_aula_brq_android/MainActivity.kt
+++ app/src/main/java/br/com/brq/projeto_aula_brq_android/MainActivity.kt
@@ -4,11 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import br.com.brq.projeto_aula_brq_android.OrientaçãoObjeto.Cor
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
+
+        fun pintar(cor: Cor){
+            println("Pintar com a cor ${cor}")
+        }
+
+
+
+
 
 
         val botao = findViewById<Button>(R.id.botao)
---
+++ app/src/main/java/br/com/brq/projeto_aula_brq_android/OrientaçãoObjeto/Cor.kt
@@ -0,0 +1,4 @@
+package br.com.brq.projeto_aula_brq_android.OrientaçãoObjeto
+
+open class Cor(open val nome: String) {
+}
